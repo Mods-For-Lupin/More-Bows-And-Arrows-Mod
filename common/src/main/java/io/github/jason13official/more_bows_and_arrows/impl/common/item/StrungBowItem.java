026a611315734d1f3690ed961322209b32049801
jason13official <jason13official@gmail.com>
2026-05-03 23:29:52 -0400

    impl stats

--- common/src/main/java/io/github/jason13official/more_bows_and_arrows/impl/common/item/StrungBowItem.java
+++ common/src/main/java/io/github/jason13official/more_bows_and_arrows/impl/common/item/StrungBowItem.java
@@ -22,6 +22,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
@@ -36,18 +37,18 @@ public class StrungBowItem extends BowItem {
     super(properties);
   }
 
-  public static float getPowerForTime(ItemStack stack, RegistryAccess registryAccess, int timeHeld) {
-
-    BowStats stats = BowAssembler.assemble(stack, registryAccess);
-    float drawSpeed = stats.get(BowStat.DRAW_SPEED);
-
+  public static float getPowerForTime(float drawSpeed, int timeHeld) {
     float pow = (float) timeHeld / (20.0F * drawSpeed);
     pow = (pow * pow + pow * 2.0F) / 3.0F;
     if (pow > 1.0F) {
       pow = 1.0F;
     }
-
     return pow;
+  }
+
+  public static float getPowerForTime(ItemStack stack, RegistryAccess registryAccess, int timeHeld) {
+    BowStats stats = BowAssembler.assemble(stack, registryAccess);
+    return getPowerForTime(stats.get(BowStat.DRAW_SPEED), timeHeld);
   }
 
   @Override
@@ -82,20 +83,22 @@ public class StrungBowItem extends BowItem {
       if (projectile.isEmpty()) {
         return false;
       } else {
+        BowStats stats = BowAssembler.assemble(itemStack, level.registryAccess());
         int timeHeld = this.getUseDuration(itemStack, entity) - remainingTime;
-        float pow = getPowerForTime(itemStack, level.registryAccess(), timeHeld);
+        float pow = getPowerForTime(stats.get(BowStat.DRAW_SPEED), timeHeld);
         if ((double) pow < 0.1) {
           return false;
         } else {
           List<ItemStack> firedProjectiles = draw(itemStack, projectile, player);
-          if (level instanceof ServerLevel serverLevel) {
-            if (!firedProjectiles.isEmpty()) {
-              this.shoot(serverLevel, player, player.getUsedItemHand(), itemStack, firedProjectiles, pow * 3.0F, 1.0F, pow == 1.0F, null);
-            }
+          if (level instanceof ServerLevel serverLevel && !firedProjectiles.isEmpty()) {
+            float arrowSpeed = pow * 3.0F * stats.get(BowStat.VELOCITY);
+            float inaccuracy = 1.0F / stats.get(BowStat.ACCURACY);
+            this.shoot(serverLevel, player, player.getUsedItemHand(), itemStack, firedProjectiles, arrowSpeed, inaccuracy, pow == 1.0F, null);
           }
 
-          level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F,
-              1.0F / (level.getRandom().nextFloat() * 0.4F + 1.2F) + pow * 0.5F);
+          float shotVolume = 1.0F / stats.get(BowStat.STEALTH);
+          level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS,
+              shotVolume, 1.0F / (level.getRandom().nextFloat() * 0.4F + 1.2F) + pow * 0.5F);
           player.awardStat(Stats.ITEM_USED.get(this));
           return true;
         }
@@ -103,6 +106,17 @@ public class StrungBowItem extends BowItem {
     } else {
       return false;
     }
+  }
+
+  @Override
+  protected Projectile createProjectile(Level level, LivingEntity shooter, ItemStack weapon, ItemStack projectile, boolean isCrit) {
+    AbstractArrow arrow = (AbstractArrow) super.createProjectile(level, shooter, weapon, projectile, isCrit);
+    BowStats stats = BowAssembler.assemble(weapon, level.registryAccess());
+    arrow.setBaseDamage(2.0 * stats.get(BowStat.DAMAGE));
+    if (!arrow.isCritArrow() && level.getRandom().nextFloat() < stats.get(BowStat.CRIT_CHANCE)) {
+      arrow.setCritArrow(true);
+    }
+    return arrow;
   }
 
   @Override
